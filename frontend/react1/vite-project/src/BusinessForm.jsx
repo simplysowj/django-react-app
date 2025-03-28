db8115daabaa1ddf6678c8aa0bfdbda89a6bfabd
sowjanya <90475451+simplysowj@users.noreply.github.com>
2025-03-28 14:26:31 -0400

    Update BusinessForm.jsx

--- frontend/react1/vite-project/src/BusinessForm.jsx
+++ frontend/react1/vite-project/src/BusinessForm.jsx
@@ -22,7 +22,7 @@ const BusinessForm = () => {
 
   const fetchBusinesses = async () => {
     try {
-      const response = await axios.get("http://127.0.0.1:8000/api/business/business/");
+      const response = await axios.get("https://djangoappcontainer2025unique.azurewebsites.net/api/business/business/");
       setBusinesses(response.data);
     } catch (error) {
       console.error("Error fetching businesses:", error);
