b2eb078d4b56ad1c16228de631c2361222cdd7ed
sowjanya <90475451+simplysowj@users.noreply.github.com>
2025-04-24 12:43:55 -0400

    Update Login.jsx

--- frontend/react1/vite-project/src/components/Login.jsx
+++ frontend/react1/vite-project/src/components/Login.jsx
@@ -15,7 +15,7 @@ const Login = ({ setIsAuthenticated }) => {
     e.preventDefault();
     try {
       const response = await axios.post(
-        "http://127.0.0.1:8000/api/business/login/",
+        "https://djangoappcontainer2025unique.azurewebsites.net/api/business/login/",
         { username, password }
       );
       
